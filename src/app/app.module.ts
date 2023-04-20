33a0a76007cccb57c59d4441c629615ed57ad598
Rafael Horochovec <rl.horochovec@gmail.com>
2023-04-20 10:15:00 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddHeroComponent } from './components/add-hero/add-hero.component';
+import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddHeroComponent,
+    HeroDetailsComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
