7363ffc8cbbe9b8207746182467aa2e8009ae3ca
Rafael Horochovec <rl.horochovec@gmail.com>
2023-04-20 16:52:02 -0300

    List/Details componentes

--- src/app/components/hero-details/hero-details.component.ts
+++ src/app/components/hero-details/hero-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HeroService } from 'src/app/services/hero.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hero } from 'src/app/models/hero.model';
 
 @Component({
   selector: 'app-hero-details',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() viewMode = false;
+
+  @Input() currentHero: Hero = {
+    name: '',
+    civil: '',
+    universe: '',
+    image: '',
+    imagePath: ''
+  };
+
+  message = '';
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    if (!this.viewMode) {
+      this.message = '';
+      this.getHero(this.route.snapshot.params["id"]);
+    }
   }
 
+  getHero(id: string): void {
+    this.heroService.get(id)
+      .subscribe({
+        next: (data) => {
+          this.currentHero = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  updateHero(): void {
+    this.message = '';
+
+    this.heroService.update(this.currentHero.id, this.currentHero)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.message = res.message ? res.message : 'This hero was updated successfully!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteHero(): void {
+    this.heroService.delete(this.currentHero.id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/heroes']);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
--- src/app/components/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes-list/heroes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/models/hero.model';
+import { HeroService } from 'src/app/services/hero.service';
 
 @Component({
   selector: 'app-heroes-list',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesListComponent implements OnInit {
 
-  constructor() { }
+  heroes?: Hero[];
+  currentHero: Hero = {};
+  currentIndex = -1;
+  title = '';
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.retrieveHeroes();
   }
 
+  retrieveHeroes(): void {
+    this.heroService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.heroes = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  refreshList(): void {
+    this.retrieveHeroes();
+    this.currentHero = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveHero(hero: Hero, index: number): void {
+    this.currentHero = hero;
+    this.currentIndex = index;
+  }
 }
